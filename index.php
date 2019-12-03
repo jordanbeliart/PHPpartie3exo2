1456f2264f1ff1b691fe703d367f2c26a23b47c5
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-03 10:52:34 +0100

    exo2P3

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$months = array (1=>'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP partie3 exo2</title>
+  </head>
+  <body>
+    <?php
+      echo $months[5];
+    ?>
+  </body>
+</html>
